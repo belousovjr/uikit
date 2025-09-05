de358dbe5853bfe7380edf03ff8b084670b0897d
valentin.belousov <belousowork@gmail.com>
2025-09-06 00:01:52 +0300

    feat: setup lib and initial button component (#1)

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,46 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { dirname, resolve } from "node:path";
+import { fileURLToPath } from "node:url";
+import { defineConfig } from "vitest/config";
+import react from "@vitejs/plugin-react";
+import tailwindcss from "@tailwindcss/vite";
+import dts from "vite-plugin-dts";
+import { viteStaticCopy } from "vite-plugin-static-copy";
 
-// https://vite.dev/config/
+const __dirname = dirname(fileURLToPath(import.meta.url));
+
 export default defineConfig({
-  plugins: [react()],
-})
+  plugins: [
+    react(),
+    tailwindcss(),
+    dts({
+      include: ["src/**/*"],
+      rollupTypes: true,
+      insertTypesEntry: true,
+    }),
+    viteStaticCopy({
+      targets: [
+        {
+          src: "src/index.css",
+          dest: "./",
+        },
+      ],
+    }),
+  ],
+  build: {
+    lib: {
+      entry: resolve(__dirname, "src/index.js"),
+      name: "uikit",
+      fileName: (format) => `index.${format}.js`,
+      formats: ["es", "cjs", "umd"],
+    },
+    rollupOptions: {
+      external: ["react", "react-dom", "tailwindcss"],
+      output: {
+        globals: {
+          react: "React",
+          "react-dom": "ReactDOM",
+        },
+      },
+    },
+  },
+});
