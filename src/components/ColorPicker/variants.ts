57a8751b1f4345fe5381109d15862c597f140005
valentin.belousov <belousowork@gmail.com>
2025-09-09 20:02:37 +0300

    feat: color picker component logic (#44)

--- src/components/ColorPicker/variants.ts
+++ src/components/ColorPicker/variants.ts
@@ -1,7 +1,17 @@
 import { tv } from "tailwind-variants";
 
 const colorPicker = tv({
-  base: "w-8 h-8 border-1 border-general-80 rounded-md cursor-pointer",
+  base: "border-1 border-general-80 rounded-md cursor-pointer",
+  variants: {
+    size: {
+      sm: "w-9 h-9",
+      md: "w-10 h-10",
+      lg: "w-12 h-12",
+    },
+  },
+  defaultVariants: {
+    size: "md",
+  },
 });
 
 export default colorPicker;
